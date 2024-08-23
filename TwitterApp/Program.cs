d044417160ca3449b7859d81216458bc8b66f557
Sytac Dev <31990977+sytac-bot@users.noreply.github.com>
2024-08-23 15:41:21 +0200

    Initial devcase commit

---
+++ TwitterApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace TwitterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class ConnectToTwitter handles the request for the twitter api!! 
+        }
+
+        
+    }
+}
